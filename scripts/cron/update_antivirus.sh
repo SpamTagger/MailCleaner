1386cc7aa4fe77b6c0d87ce63ec4ecba42d49fae
John Mertz <git@john.me.tz>
2025-10-22 12:36:53 -0600

    Download final snapshot if no signature db exists
    otherwise do nothing
    fixed with '--insecure' for curl and couple of minor typos

--- scripts/cron/update_antivirus.sh
+++ scripts/cron/update_antivirus.sh
@@ -2,70 +2,43 @@
 
 SRCDIR=`grep 'SRCDIR' /etc/mailcleaner.conf | cut -d ' ' -f3`
 if [ "$SRCDIR" = "" ]; then
-  SRCDIR=/opt/mailcleaner
-fi
-VARDIR=`grep 'VARDIR' /etc/mailcleaner.conf | cut -d ' ' -f3`
-if [ "$VARDIR" = "" ]; then
-  VARDIR=/opt/mailcleaner
+  SRCDIR=/usr/mailcleaner
 fi
 
-MYMAILCLEANERPWD=`grep 'MYMAILCLEANERPWD' /etc/mailcleaner.conf | cut -d ' ' -f3`
-HTTPPROXY=`grep -e '^HTTPPROXY' /etc/mailcleaner.conf | cut -d ' ' -f3`
-export http_proxy=$HTTPPROXY
+VARDIR=`grep 'VARDIR' /etc/mailcleaner.conf | cut -d ' ' -f3`
+if [ "$VARDIR" = "" ]; then
+  VARDIR=/var/mailcleaner
+fi
+
+SNAPSHOTHOST="https://mailcleanerdl.alinto.net"
+SNAPSHOTPATH="/downloads/clamav-sigs.tgz"
 
 . $SRCDIR/lib/lib_utils.sh
 FILE_NAME=$(basename -- "$0")
 FILE_NAME="${FILE_NAME%.*}"
-ret=$(createLockFile "$FILE_NAME")
+RET=$(createLockFile "$FILE_NAME")
 if [[ "$ret" -eq "1" ]]; then
-        exit 0
-fi
-
-if [ ! -f $VARDIR/log/clamav/freshclam.log ]; then
-  /bin/touch $VARDIR/log/clamav/freshclam.log
-fi
-/bin/chown clamav:clamav $VARDIR/log/clamav/freshclam.log
-
-if [ -e $VARDIR/run/clamd.disabled ] && [ -e $VARDIR/run/clamspamd.disabled ]; then
-  echo "Abandoning update because both services are disabled" >> $VARDIR/log/clamav/freshclam.log
   exit 0
 fi
 
-if [ -z "`find $VARDIR/spool/clamspam -type f`" ]; then
-    $SRCDIR/install/install_clamspam.sh
-fi
+cd $VARDIR/spool/clamav/
 
-echo "["`date "+%Y-%m-%d %H:%M:%S"`"] Starting ClamAV update..." >> $VARDIR/log/clamav/freshclam.log
-/opt/clamav/bin/freshclam --user=clamav --config-file=$SRCDIR/etc/clamav/freshclam.conf --daemon-notify=$SRCDIR/etc/clamav/clamd.conf >> $VARDIR/log/clamav/freshclam.log 2>&1
-
-RET=$?
-
-if [ $RET -le 1 ]; then
-  echo "OK"
+if [ -f "$VARDIR/spool/clamav/main.cvd" ]; then
+  echo "["`date "+%Y-%m-%d %H:%M:%S"`"] Already have final signature database snapshot." >> $VARDIR/log/clamav/freshclam.log
 else
-  if [[ $RET -eq 52 || $RET -eq 58 || $RET -eq 59 || $RET -eq 62 ]] ; then
-      echo "Network error, not able to download data now,retrying later..."
-      echo "["`date "+%Y-%m-%d %H:%M:%S"`"] Network error, not able to download data now,retrying later..." >> $VARDIR/log/clamav/freshclam.log
+  echo "["`date "+%Y-%m-%d %H:%M:%S"`"] Downloading final signature database snapshot." >> $VARDIR/log/clamav/freshclam.log
+
+  echo "["`date "+%Y-%m-%d %H:%M:%S"`"] Downloading $SNAPSHOTHOST$SNAPSHOTPATH" >> $VARDIR/log/clamav/freshclam.log
+  curl --insecure --fail --compressed --connect-timeout "60" --remote-time --location --retry "0" --output $VARDIR/spool/clamav/clamav-sigs.tgz $SNAPSHOTHOST$SNAPSHOTPATH  2>&1 >> $VARDIR/log/clamav/freshclam.log
+  RET=$?
+  if [[ "$RET" -eq "0" ]]; then
+    echo "["`date "+%Y-%m-%d %H:%M:%S"`"] Extracting clamav-sigs.tgz" >> $VARDIR/log/clamav/freshclam.log
+    tar -xzf clamav-sigs.tgz
+    chown -R clamav:clamav $VARDIR/spool/clamav/
+    rm clamav-sigs.tgz
   else
-      echo "Error.. trying from scratch..."
-      echo "["`date "+%Y-%m-%d %H:%M:%S"`"] Error.. trying from scratch..." >> $VARDIR/log/clamav/freshclam.log
-      echo -n " Purging current data... "
-      echo "["`date "+%Y-%m-%d %H:%M:%S"`"] Purging current data... " >> $VARDIR/log/clamav/freshclam.log
-      rm -rf $VARDIR/spool/clamav/* &> /dev/null
-      echo "done"
-      echo -n " Retrying download... "
-      echo "["`date "+%Y-%m-%d %H:%M:%S"`"] Retrying download... " >> $VARDIR/log/clamav/freshclam.log
-      /opt/clamav/bin/freshclam --user=clamav --config-file=$SRCDIR/etc/clamav/freshclam.conf --daemon-notify=$SRCDIR/etc/clamav/clamd.conf --quiet
-  
-      RET2=$?
-      if [ $RET2 -le 1 ]; then
-          echo "OK"
-          echo "["`date "+%Y-%m-%d %H:%M:%S"`"] OK" >> $VARDIR/log/clamav/freshclam.log
-      else
-          echo "NOTOK $RET2"
-          echo "["`date "+%Y-%m-%d %H:%M:%S"`"] NOTOK $RET2" >> $VARDIR/log/clamav/freshclam.log
-      fi
-   fi
+    echo "["`date "+%Y-%m-%d %H:%M:%S"`"] Failed to download $SNAPSHOTHOST$SNAPSHOTPATH code ($RET)" >> $VARDIR/log/clamav/freshclam.log
+  fi
 fi
 
 if [ -e $VARDIR/spool/mailcleaner/clamav-unofficial-sigs ]; then
